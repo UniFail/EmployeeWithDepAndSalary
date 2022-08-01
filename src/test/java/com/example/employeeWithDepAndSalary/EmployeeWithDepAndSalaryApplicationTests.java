3d52b0a8bb7e06fa2dd60b8d516feddd158eefa6
UniFail <vika.lolkova@mail.ru>
2022-08-02 00:23:42 +0700

    Initial commit

---
+++ src/test/java/com/example/employeeWithDepAndSalary/EmployeeWithDepAndSalaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeWithDepAndSalary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWithDepAndSalaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
